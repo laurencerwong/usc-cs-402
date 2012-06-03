f7dec061661b191ab55beaf52e87da8e2cf655eb
rpuncel@gmail.com <rpuncel@gmail.com@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-06-03 16:50:34 +0000

    Added a ton of comments

--- trunk/code/threads/synch.cc
+++ trunk/code/threads/synch.cc
@@ -113,6 +113,7 @@ Lock::Lock(char* debugName) {
 //--------------------------------------------------------------
 Lock::~Lock() {
 	delete queue;
+	delete name;
 }
 //----------------------------------------------------------------
 
@@ -123,9 +124,11 @@ Lock::~Lock() {
 //----------------------------------------------------------------
 void Lock::Acquire() {
 
-	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts so the current thread
+														//cannot be switched out
 	
-  if(isHeldByCurrentThread()){ //thread already has possession
+  if(isHeldByCurrentThread()){ //thread already has possession, if allowed to acquire again,
+	  	  	  	  	  	  	  //thread would be blocked entire process indefinitely since interrupts are switched off
   	(void) interrupt->SetLevel(oldLevel);		//so restore interrupts
   	return;																	//and do not wait
   }
@@ -143,24 +146,28 @@ void Lock::Acquire() {
 void Lock::Release() {
 	Thread* thread;
 	IntStatus oldLevel = interrupt->SetLevel(IntOff);
-	if(!isHeldByCurrentThread()){
+	if(!isHeldByCurrentThread()){ //shouldn't allow synchronization to be messed up if a thread calls release accidentally or maliciously
 		printf("Thread %s calling Release() on Lock %s does not have lock\n", currentThread->getName(), name);
 		(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
 		return; //kicks out a thread that didn't acquire Lock
 	}
-	if(!queue->IsEmpty()){
+	if(!queue->IsEmpty()){ //check this condition to avoid segmentation faults or bus errors
 		thread = (Thread *) queue->Remove();
-		threadWithPossession = thread;
-		scheduler->ReadyToRun(thread);
+		threadWithPossession = thread; //give next thread immediate possession of the lock
+		scheduler->ReadyToRun(thread); //add to the system's ready queue
 	}
-	else{
+	else{ //make sure lock can be grabbed by anyone if no one was waiting in te ready queue
 		state = FREE;
 		threadWithPossession = NULL;
 	}
-	(void) interrupt->SetLevel(oldLevel);
+	(void) interrupt->SetLevel(oldLevel); //restore interrupts
 	
 }
 
+//--------------------------------------------------------------
+//Allows us to use the external system variable Thread* currentThread
+//to check against the thread that last acquired the lock.
+//---------------------------------------------------------
 bool Lock::isHeldByCurrentThread(){
 	return currentThread == threadWithPossession;
 }
@@ -174,43 +181,55 @@ Condition::~Condition() {
 }
 void Condition::Wait(Lock* conditionLock) { 
 	
-	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts so this operation is atomic
+														//(can't be switched out)
+	//a null argument would be improper, so don't allow current thread to execute rest of function
 	if(conditionLock == NULL){
 		printf("Argument passed to Condition::Wait() was null, returning without performing wait\n");
 		(void) interrupt->SetLevel(oldLevel);
 		return;
 	}
+	//set the current lock for the condition variable if we don't already remember it
 	if(waitingLock == NULL){
 		waitingLock = conditionLock;
 	}
+	//check that the lock passed in is the lock associated with this CV.  Otherwise, it would be improper
+	//to allow the thread to release the lock, and would mess up the semantics of the CV
 	if(conditionLock != waitingLock){
 		printf("Argument passed to Condition::Wait() was the incorrect lock, returning without performing wait\n");
 		(void) interrupt->SetLevel(oldLevel);
 	}
 	//OK to wait
-	conditionLock->Release();
+
+	conditionLock->Release(); //release lock
 	queue->Append((void *) currentThread);
-	currentThread->Sleep();
-	conditionLock->Acquire();
-  (void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
+	currentThread->Sleep(); //will sleep. current thread will be not regain processor until it is at the front of queue
+							//and Signal is called on this CV
+	conditionLock->Acquire(); //reacquire lock before returning ensures mutual exclusivity until current thread manually releases lock
+  (void) interrupt->SetLevel(oldLevel);	// re-enable interrupts (atomic operation finished)
   
 }
 void Condition::Signal(Lock* conditionLock) { 
 	Thread *thread;
-	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts
-	if(queue->IsEmpty()) return;
-	if(waitingLock != conditionLock){
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);	// disable interrupts so this opertion is atomic
+														//(can't be switch out
+	if(queue->IsEmpty()) return; //don't need to worry if an extraneous call to signal, just return
+	if(waitingLock != conditionLock){ //check that it is the proper lock, otherwise the thread woken up will try
+										//to acquire a lock that may not be free and be blocked as a result
 		printf("Argument passed to Condition::Signal() was the incorrect lock, returning without performing wait\n");
-		(void) interrupt->SetLevel(oldLevel);
+		(void) interrupt->SetLevel(oldLevel); //restore interrupts (atomic operation done)
 		return;
 	}
-	thread = (Thread *) queue->Remove();
-	scheduler->ReadyToRun(thread);
+	thread = (Thread *) queue->Remove(); //get the next thread waiting on CV
+	scheduler->ReadyToRun(thread); //add to system's ready queue
 	if(queue->IsEmpty()){
-		waitingLock = NULL;
+		waitingLock = NULL; //if queue is empty, we no longer need to remember a lock
+							//and can allow a different lock to be passed in on the next Wait
 	}
 	(void) interrupt->SetLevel(oldLevel);	// re-enable interrupts
 }
 void Condition::Broadcast(Lock* conditionLock) { 
-	while(!queue->IsEmpty()) Signal(conditionLock);
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);
+	while(!queue->IsEmpty()) Signal(conditionLock); //all operations we need are in Signal, just need to call over and over
+	(void) interrupt->SetLevel(oldLevel);
 }
