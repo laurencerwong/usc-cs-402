94534153fecf86d0456b429f880453a8e83f8162
keithderuiter <keithderuiter@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-05 22:57:49 +0000

    Added some debugging prints to evict

--- code/threads/system.h
+++ code/threads/system.h
@@ -36,7 +36,7 @@ extern void Initialize(int argc, char **argv); 	// Initialization,
 extern void Cleanup();				// Cleanup, called when
 						// Nachos is done.
 
-#ifdef CHANGED
+//#ifdef CHANGED
 
 struct ProcessEntry {
 	int numThreadsAlive;
@@ -72,7 +72,7 @@ enum EvictionPolicy {FIFO, RAND};
 extern EvictionPolicy evictionPolicy;
 
 
-#endif
+//#endif
 
 extern Thread *currentThread;			// the thread holding the CPU
 extern Thread *threadToBeDestroyed;  		// the thread that just finished
