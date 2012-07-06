815846ab4413dc6a0e9815e34a50b491c233c98c
laurencerwong@gmail.com <laurencerwong@gmail.com@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-06 22:08:35 +0000

    fixed test_fork_matmult

--- code/test/test_fork_matmult.c
+++ code/test/test_fork_matmult.c
@@ -1,34 +1,29 @@
 #include "../userprog/syscall.h"
 
 
-//include "syscall.h"
-
-#define Dim 	20	/* sum total of the arrays doesn't fit in
-			 * physical memory
-			 */
 
 
 
 int matMult()
 {
-	int A[Dim][Dim];
-	int B[Dim][Dim];
-	int C[Dim][Dim];
+	int A[20][20];
+	int B[20][20];
+	int C[20][20];
 	int i, j, k;
 
-	for (i = 0; i < Dim; i++)		/* first initialize the matrices */
-		for (j = 0; j < Dim; j++) {
+	for (i = 0; i < 20; i++)		/* first initialize the matrices */
+		for (j = 0; j < 20; j++) {
 			A[i][j] = i;
 			B[i][j] = j;
 			C[i][j] = 0;
 		}
 
-	for (i = 0; i < Dim; i++)		/* then multiply them together */
-		for (j = 0; j < Dim; j++)
-			for (k = 0; k < Dim; k++)
+	for (i = 0; i < 20; i++)		/* then multiply them together */
+		for (j = 0; j < 20; j++)
+			for (k = 0; k < 20; k++)
 				C[i][j] += A[i][k] * B[k][j];
 
-	Exit(C[Dim-1][Dim-1]);		/* and then we're done */
+	Exit(C[20-1][20-1]);		/* and then we're done */
 }
 
 int main(){
