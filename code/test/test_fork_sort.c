a1588471d8f7890123ff6f594099ff6ecea1a11f
keithderuiter <keithderuiter@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-06 23:32:37 +0000

    Fixed test_fork_sort

--- code/test/test_fork_sort.c
+++ code/test/test_fork_sort.c
@@ -7,33 +7,53 @@
  *	and store the result back to the file system!
  */
 
-#include "syscall.h"
+#include "../userprog/syscall.h"
+#define SIZE 1024
+int A[SIZE];	/* size of physical memory; with code, we'll run out of space!*/
+int B[SIZE];	/* size of physical memory; with code, we'll run out of space!*/
+
+int sortA()
+{
+	int i, j, tmp;
+
+	/* first initialize the array, in reverse sorted order */
+	for (i = 0; i < SIZE; i++)
+		A[i] = SIZE - i;
+
+	/* then sort! */
+	for (i = 0; i < SIZE - 1; i++)
+		for (j = i; j < (SIZE - 1 - i); j++)
+			if (A[j] > A[j + 1]) {	/* out of order -> need to swap ! */
+				tmp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = tmp;
+			}
+	Exit(A[0]);		/* and then we're done -- should be 0! */
+}
+
+int sortB()
+{
+	int i, j, tmp;
+
+	/* first initialize the array, in reverse sorted order */
+	for (i = 0; i < SIZE; i++)
+		B[i] = SIZE - i;
+
+	/* then sort! */
+	for (i = 0; i < SIZE - 1; i++)
+		for (j = i; j < (SIZE - 1 - i); j++)
+			if (B[j] > B[j + 1]) {	/* out of order -> need to swap ! */
+				tmp = B[j];
+				B[j] = B[j + 1];
+				B[j + 1] = tmp;
+			}
+	Exit(B[0]);		/* and then we're done -- should be 0! */
+}
 
 
 int main() {
-	Fork(sort, "Sort 1", sizeof("Sort 1"));
-	Fork(sort, "Sort 2", sizeof("Sort 2"));
-
+	Fork(sortA, "sort_1", sizeof("sort_1"));
+	Fork(sortB, "sort_2", sizeof("sort_2"));
 	Exit(0);
 }
 
-
-int sort()
-{
-	int A[1024];	/* size of physical memory; with code, we'll run out of space!*/
-    int i, j, tmp;
-
-    /* first initialize the array, in reverse sorted order */
-    for (i = 0; i < 1024; i++)
-        A[i] = 1024 - i;
-
-    /* then sort! */
-    for (i = 0; i < 1023; i++)
-        for (j = i; j < (1023 - i); j++)
-	   if (A[j] > A[j + 1]) {	/* out of order -> need to swap ! */
-	      tmp = A[j];
-	      A[j] = A[j + 1];
-	      A[j + 1] = tmp;
-    	   }
-    Exit(A[0]);		/* and then we're done -- should be 0! */
-}
