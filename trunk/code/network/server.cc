b4505fd71560337564c41b82766ca5005a59b605
keithderuiter <keithderuiter@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-09 21:13:25 +0000

    Added server execution finctions for the distributed calls, also added server messaging.  format of create MV updated to include initial value.

--- trunk/code/network/server.cc
+++ trunk/code/network/server.cc
@@ -13,25 +13,37 @@
 
 #define MAX_SERVER_LOCKS 500
 #define MAX_SERVER_CVS 500
+#define MAX_SERVER_MV_ARRAYS 500
+#define MAX_SERVER_ENTRIES_PER_MV_ARRAY 500
 
-typedef struct ServerLockEntry{
+
+struct ServerLockEntry{
 	ServerLock *lock;
 	bool isToBeDeleted;
 	int acquireCount;
-} LockEntry;
+};
 
-typedef struct ServerConditionEntry{
+struct ServerConditionEntry{
 	ServerCondition *condition;
 	bool isToBeDeleted;
-} ConditionEntry;
+};
+
+struct ServerMVEntry {
+	int *mvEntries;
+	int numEntries;
+	char *name;
+};
 
 BitMap *serverLockMap;
 BitMap *serverConditionMap;
+BitMap *serverMVMap;
 
 int serverLockArraySize = 0;
 int serverConditionArraySize = 0;
-ServerLockEntry *severLockTable;
+int serverMVArraySize = 0;
+ServerLockEntry *serverLockTable;
 ServerConditionEntry *serverConditionTable;
+ServerMVEntry *serverMVTable;
 
 int ServerCreateLock(int machineID, int mailboxID, char* name) {
 	if(lockArraySize == 0){ //instantiate lockTable on first call to CreateLock_Syscall
@@ -71,11 +83,256 @@ void ServerDestroyLock(int lockIndex) {
 		serverLockMap->Clear(lockIndex);
 	}
 	else{ //some thread is depending on the thread being there, so just defer deletion
-		lockTable[lockIndex].isToBeDeleted = true;
+		serverLockTable[lockIndex].isToBeDeleted = true;
 	}
 }
 
+ClientRequest* ServerAcquire(int machineID, int mailbox, int lockIndex) {
+	if(lockIndex < 0 || lockIndex > serverLockArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Acquire with an invalid index %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
+	if(!lockMap->Test(lockIndex)){ //lock has not been instantiated at this index
+		printf("Thread %s called Acquire on a lock that does not exist: %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
 
+	//validation done, ok to perform operation
+	serverLockTable[lockIndex].acquireCount++; //marks this lock as being held by a user thread, protects lock from deletion til same thread call Release syscall
+	return serverLockTable[lockIndex].lock->Acquire(new ClientRequest(machineID, mailbox));
+}
+
+ClientRequest* ServerRelease(int machineID, int mailbox, int lockIndex){
+
+	if(lockIndex < 0 || lockIndex > serverLockArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Release with an invalid index %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
+	if(!serverLockMap->Test(lockIndex)){ //lock has not been instantiated at this index
+		printf("Thread %s called Release on a lock that does not exist: %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
+
+	//validation done, ok to perform operation
+	serverLockTable[lockIndex].acquireCount--; //decrease count since currentThread no longer depends on lock
+	ClientRequest* rv =  serverLockTable[lockIndex].lock->Release(new ClientRequest(machineID, mailbox));
+	if(serverLockTable[lockIndex].isToBeDeleted && serverLockTable[lockIndex].acquireCount == 0){ //check if needs to be deleted, delete if no one is waiting
+		if(!(serverLockTable[lockIndex].lock->isBusy())){
+			delete serverLockTable[lockIndex].lock;
+			serverLockMap->Clear(lockIndex);
+		}
+	}
+	return rv;
+}
+
+int ServerCreateCV(int machineID, int mailbox, char* name){
+
+	if(serverConditionArraySize == 0){ //should only execute on first call to this function in a run of nachos
+		serverConditionTable = new ServerConditionEntry[MAX_SERVER_CONDITIONS];
+		serverConditionMap = new BitMap(MAX_SERVER_CONDITIONS);
+		serverConditionArraySize = MAX_SERVER_CONDITIONS;
+	}
+	int nextFreeIndex = serverConditionMap->Find();
+	if(nextFreeIndex == -1){ //error, kernel is out of memory for CVs and should terminate
+		printf("Fatal system error, too many condition variables! Shut it down!\n");
+		//interrupt->Halt();
+		//on the server, don't halt, send a -1 as CV index response
+	}
+	else {
+		printf("Creating %s CV of index of %d\n", name, nextFreeIndex);
+		//initialize the conditionTable entry we grabbed for this CV
+		serverConditionTable[nextFreeIndex].condition = new ServerCondition (name);
+		serverConditionTable[nextFreeIndex].isToBeDeleted = false;
+	}
+	return nextFreeIndex;
+}
+
+void ServerDestroyCV(int conditionIndex) {
+	if(conditionIndex < 0 || conditionIndex > serverConditionArraySize -1){ //array index is out of bounds
+		printf("Thread %s tried to delete a condition with an invalid index %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+	if(!serverConditionMap->Test(conditionIndex)){ //condition has not been instantiated at this index
+		printf("Thread %s tried to delete a condition that does not exist: %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+
+	//validation done, ok to perform operation
+	if(!(serverConditionTable[conditionIndex].condition->hasWaiting())){ //okay to delete if no threads in condition's queue
+		delete serverConditionTable[conditionIndex].condition;
+		serverConditionMap->Clear(conditionIndex);
+	}
+	else{ //if someone is in condition's queue, defer deletion
+		serverConditionTable[conditionIndex].isToBeDeleted = true;
+	}
+}
+
+ClientRequest* ServerSignal(int machineID, int mailbox, int conditionIndex, int lockIndex){
+	if(conditionIndex < 0 || conditionIndex > serverConditionArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Signal with an invalid index %d\n", currentThread->getName(), conditionIndex);
+		return NULL;
+	}
+	if(!serverConditionMap->Test(conditionIndex)){ //condition has not been instantiated at this index
+		printf("Thread %s called Signal on a condition that does not exist: %d\n", currentThread->getName(), conditionIndex);
+		return NULL;
+	}
+	if(lockIndex < 0 || lockIndex > serverLockArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Signal with an invalid lock index %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
+	if(!serverLockMap->Test(lockIndex)){ //lock has not been instantiated at this index
+		printf("Thread %s called Signal on a lock that does not exist: %d\n", currentThread->getName(), lockIndex);
+		return NULL;
+	}
+
+	//validation done, ok to perform operation
+	return serverConditionTable[conditionIndex].condition->Signal(serverLockTable[lockIndex].lock, new ClientRequest(machineID, mailbox));
+}
+
+void ServerWait(int machineID, int mailbox, int conditionIndex, int lockIndex){
+
+	if(conditionIndex < 0 || conditionIndex > serverConditionArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Wait with an invalid index %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+	if(!serverConditionMap->Test(conditionIndex)){ //condition has not been instantiated at this index
+		printf("Thread %s called Wait on a condition that does not exist: %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+	if(lockIndex < 0 || lockIndex > serverLockArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Wait with an invalid lock index %d\n", currentThread->getName(), lockIndex);
+		return;
+	}
+	if(!serverLockMap->Test(lockIndex)){ //lock has not been instantiated at this index
+		printf("Thread %s called Wait with a lock index that does not exist: %d\n", currentThread->getName(), lockIndex);
+		return;
+	}
+
+	//validation done, ok to perform operation
+	serverConditionTable[conditionIndex].condition->Wait(serverLockTable[lockIndex].lock, new ClientRequest(machineID, mailbox));
+	//need to check if the condition has been marked for deletion.  if it has,
+	//okay to delete if no one is currently in the condition's queue (this is an atomic operation, so no thread is in the middle of calling wait right now either)
+	if(serverConditionTable[conditionIndex].isToBeDeleted){
+		if(!(serverConditionTable[conditionIndex].condition->hasWaiting())){
+			delete serverConditionTable[conditionIndex].condition;
+			serverConditionMap->Clear(conditionIndex);
+		}
+	}
+}
+
+void ServerBroadcast(int machineID, int mailbox, int conditionIndex, int lockIndex){
+
+	if(conditionIndex < 0 || conditionIndex > conditionArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Broadcast with an invalid index %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+	if(!conditionMap->Test(conditionIndex)){ //condition has not been instantiated at this index
+		printf("Thread %s called Broadcast on a condition that does not exist: %d\n", currentThread->getName(), conditionIndex);
+		return;
+	}
+	if(lockIndex < 0 || lockIndex > lockArraySize -1){ //array index is out of bounds
+		printf("Thread %s called Broadcast with an invalid lock index %d\n", currentThread->getName(), lockIndex);
+		return;
+	}
+	if(!lockMap->Test(lockIndex)){ //lock has not been instantiated at this index
+		printf("Thread %s called Broadcast with a lock that does not exist: %d\n", currentThread->getName(), lockIndex);
+		return;
+	}
+
+	//validation done, ok to perform operation
+	serverConditionTable[conditionIndex].condition->Broadcast(lockTable[lockIndex].lock);
+	//TODO- you must loop signalling everyone here, including the sending of messages!
+	//also, construct and return a blank clientrequest with response false
+}
+
+int ServerCreateMV(int machineID, int mailboxID, char* name, int numEntries, int initialValue) {
+	if(serverMVArraySize == 0){ //instantiate serverMVTable on first call to CreateMV_Syscall
+		serverMVTable = new ServerMVEntry[MAX_SERVER_MV_ARRAYS];
+		serverMVMap = new BitMap(MAX_SERVER_MV_ARRAYS);
+		serverMVArraySize = MAX_SERVER_MV_ARRAYS;
+	}
+
+	if(numEntries > MAX_SERVER_ENTRIES_PER_MV_ARRAY) {
+		printf("User program requested too many MVs!  Max is %d entries per MV array\n", MAX_SERVER_ENTRIES_PER_MV_ARRAY);
+		return -1;
+	}
+
+	int nextFreeIndex = serverMVMap->Find(); //returns a free index in lockTable
+	if(nextFreeIndex == -1){ //error, kernel is out of memory for locks and should terminate
+		printf("Out of space for user program MVs!\n");
+		//interrupt->Halt();
+		//on the server, don't halt, send a -1 as index response
+	}
+	else {
+		printf("Creating MV set %s with %d entries at index %d\n", name, numEntries, nextFreeIndex);
+		//initialize the MVtable entry
+		serverMVTable[nextFreeIndex].mvEntries = new int[numEntries];
+		serverMVTable[nextFreeIndex].numEntries = numEntries;
+		serverMVTable[nextFreeIndex].name = name;	//TODO do we need to copy or something here?
+	}
+	return nextFreeIndex;
+}
+
+void ServerDestroyMV(int mvIndex) {
+	if(mvIndex < 0 || mvIndex > serverMVArraySize - 1){ //array index is out of bounds
+		printf("Thread %s tried to delete a MV with an invalid index %d\n", currentThread->getName(), mvIndex);
+		return;
+	}
+	if(!serverMVMap->Test(mvIndex)){ //MV has not been instantiated at this index
+		printf("Thread %s tried to delete a MV that does not exist: %d\n", currentThread->getName(), mvIndex);
+		return;
+	}
+
+	//validation done, ok to perform operation
+	delete serverMVTable[mvIndex].mvEntries;
+	serverMVMap->Clear(mvIndex);
+}
+
+int ServerGetMV(int mvIndex, int entryIndex) {
+	if(mvIndex < 0 || mvIndex > serverMVArraySize - 1) {
+		printf("Thread %s tried to get a MV with an invalid index %d\n", currentThread->getName(), mvIndex);
+		return -1;
+	}
+	if(!serverMVMap->Test(mvIndex)){ //MV has not been instantiated at this index
+		printf("Thread %s tried to get a MV that does not exist: %d\n", currentThread->getName(), mvIndex);
+		return -1;
+	}
+	if(entryIndex < 0 || entryIndex > serverMVTable[mvIndex].numEntries - 1) {
+		printf("Thread %s tried to get a MV at invalid entry index %d in MV array %d\n", currentThread->getName(), entryIndex, mvIndex);
+		return -1;
+	}
+
+	return serverMVTable[mvIndex].mvEntries[entryIndex];
+}
+
+void ServerSetMV(int mvIndex, int entryIndex, int value) {
+	if(mvIndex < 0 || mvIndex > serverMVArraySize - 1) {
+		printf("Thread %s tried to set a MV with an invalid index %d\n", currentThread->getName(), mvIndex);
+		return;
+	}
+	if(!serverMVMap->Test(mvIndex)){ //MV has not been instantiated at this index
+		printf("Thread %s tried to set a MV that does not exist: %d\n", currentThread->getName(), mvIndex);
+		return;
+	}
+	if(entryIndex < 0 || entryIndex > serverMVTable[mvIndex].numEntries - 1) {
+		printf("Thread %s tried to set a MV at invalid entry index %d in MV array %d\n", currentThread->getName(), entryIndex, mvIndex);
+		return;
+	}
+
+	serverMVTable[mvIndex].mvEntries[entryIndex] = value;
+}
+
+//takes buf[0:3] and makes an int, where 0 is the MSByte of the int
+int extractInt(char *buf) {
+	return (buf[0] << 24) + (buf[1] << 16) + (buf[2] << 8) + buf[3];
+}
+
+void compressInt(int x, char dest[4]) {
+	dest[0] = (x >> 24) & 0x000000ff;
+	dest[1] = (x >> 16) & 0x000000ff;
+	dest[2] = (x >> 8) & 0x000000ff;
+	dest[3] = (x >> 0) & 0x000000ff;
+}
 
 void Server() {
 
@@ -90,71 +347,136 @@ void Server() {
 		//parse message
 		char messageType = messageData[0];
 		int machineID = packetHeader->from;
-		int mailboxNumber = mailHeader->from;
+		int mailbox = mailHeader->from;
 
 		int replyMachineID = machineID;
-		int replyMailboxNumber = mailboxNumber;
-
+		int replyMailbox = mailbox;
+		bool respond = false;
+		int replyData = 0;
 
 		//handle the message
 		switch(messageType) {	//first byte is the message type
-		case CREATE_LOCK:	//data[9] = nameLength, data[10:10+nameLength] = name
-			char nameLength = messageData[9];
+		case CREATE_LOCK:	//data[1] = nameLength, data[2:2+nameLength] = name
+			char nameLength = messageData[1];
 			char* name = new char[nameLength];
-			strncpy(name, (messageData + 10), nameLength);
-			reply = ServerCreateLock(machineID, mailboxNumber, name);
-			break;
-		case DESTROY_LOCK:
-			reply = ServerDestroyLock();
+			strncpy(name, (messageData + 2), nameLength);
+			replyData = ServerCreateLock(machineID, mailbox, name);
+			respond = true;
 			break;
 
+		case DESTROY_LOCK:
+			int lockIndex = extractInt(messageData + 1);
+			ServerDestroyLock(lockIndex);
+			break;
 
 		case ACQUIRE:
-			ClientRequest* temp = ServerAcquire(request);
+			int lockIndex = extractInt(messageData + 1);
+			ClientRequest* temp = ServerAcquire(machineID, mailbox, lockIndex);
 			if(temp->respond){
 				replyMachine = temp->machineID;
 				replyMailbox = temp->mailboxNumber;
+				respond = true;
+			}
+			//TODO- can we delete temp, or is it used internally in the lock/cv?
+			break;
+
+		case RELEASE:
+			int lockIndex = extractInt(messageData + 1);
+			ClientRequest* temp = ServerRelease(machineID, mailbox, lockIndex);
+			if(temp->respond){
+				replyMachine = temp->machineID;
+				replyMailbox = temp->mailboxNumber;
+				respond = true;
 			}
 			break;
-		case RELEASE:
-			reply = ServerRelease();
-			break;
+
 		case CREATE_CV:
-			reply = ServerCreateCV(request);
+			char nameLength = messageData[1];
+			char* name = new char[nameLength];
+			strncpy(name, (messageData + 2), nameLength);
+			replyData = ServerCreateCV(machineID, mailbox, name);
+			respond = true;
 			break;
+
 		case DESTROY_CV:
-			reply = ServerDestroyCV(request);
+			int cvToDestroy = extractInt(messageData + 1);
+			ServerDestroyCV(cvToDestroy);
 			break;
+
 		case SIGNAL:	//condition, then lock in message
-			reply = ServerSignal(request);
+			int cvIndex = extractInt(messageData + 1);
+			int lockIndex = extractInt(messageData + 5);
+			ClientRequest *temp = ServerSignal(machineID, mailbox, cvIndex, lockIndex);
+			if(temp->respond){
+				replyMachine = temp->machineID;
+				replyMailbox = temp->mailboxNumber;
+				respond = true;
+			}
 			break;
+
 		case WAIT:
-			reply = ServerWait(request);
+			int cvIndex = extractInt(messageData + 1);
+			int lockIndex = extractInt(messageData + 5);
+			ServerWait(machineID, mailbox, cvIndex, lockIndex);
 			break;
+
 		case BROADCAST:
-			reply = ServerBroadcast(request);
+			int cvIndex = extractInt(messageData + 1);
+			int lockIndex = extractInt(messageData + 5);
+			ServerBroadcast(machineID, mailbox, cvIndex, lockIndex);	//TODO, need to handle messaging back, maybe return a success, no response ClientRequest
 			break;
+
 		case CREATE_MV:
-			reply = ServerCreateMV(request);
+			int numEntries = extractInt(messageData + 1);
+			int initialValue = extractInt(messageData + 5)
+			char nameLength = messageData[9];
+			char* name = new char[nameLength];
+			strncpy(name, (messageData + 10), nameLength);
+			replyData = ServerCreateMV(machineID, mailbox, name, numEntries, initialValue);
+			respond = true;
 			break;
+
 		case DESTROY_MV:
-			reply = ServerDestroyMV(request);
+			int mvToDestroy = extractInt(messageData + 1);
+			ServerDestroyMV(mvToDestroy);
 			break;
+
 		case GET_MV:
-			reply = ServerGetMV(request);
+			int mvIndex = extractInt(messageData + 1);
+			int entryIndex = extractInt(messageData + 5);
+			replyData = ServerGetMV(mvIndex, entryIndex);
+			respond = true;
 			break;
+
 		case SET_MV:
-			reply = ServerSetMV(request);
+			int mvIndex = extractInt(messageData + 1);
+			int entryIndex = extractInt(messageData + 5);
+			int value = extractInt(messageData + 9);
+			ServerSetMV(mvIndex, entryIndex, value);
 			break;
+
 		default:
 			//oops...
 			break;
 
 		}
 
-		//send response message
+		if(respond) {
+			PacketHeader *responsePacketHeader = new PacketHeader;
+			MailHeader *responseMailHeader = new MailHeader;
+			char* responseData = new char[MaxMailSize];
 
+			responsePacketHeader->to = replyMachineID; //machine i'm responding to
+			responsePacketHeader->from = myMachineID; //this machine's number
+			mailHeader->to = replyMailbox;	//mailbox i'm responding to
+			mailHeader->from = 0; //server mailbox?  TODO
 
+			//pack message
+			data[0] = compressInt(replyData, (responseData + 0));
+
+			//send
+			bool success = postOffice->Send(*packetHeader, *mailHeader, data);
+		}
 	}
 }
 
