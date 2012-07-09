6250b2ba8f45743b37da65a43d9f2b57900f65a7
keithderuiter <keithderuiter@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-09 23:40:09 +0000

    fixed variable declaration problem

--- code/test/testmutex.c
+++ code/test/testmutex.c
@@ -11,7 +11,7 @@ int main(){
 	int counter = CreateMV("Counter", sizeof("Counter"), 1, 0);
 	int i = 0;
 	Acquire(lock);
-	SetMV(numClients, 0, GetMV(counter, 0) + 1);
+	SetMV(counter, 0, GetMV(counter, 0) + 1);
 	for(i = 0; i < 30; i++){
 		NPrint("counter value is %d\n", sizeof("counter value is %d\n"), GetMV(counter, 0));
 	}
