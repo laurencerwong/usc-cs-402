afc88ea5a8dc754a2132850cf69f34f20f8b8ba0
laurencerwong@gmail.com <laurencerwong@gmail.com@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-11 23:13:55 +0000

    added delay to testmutex and added debug statement in synch for when client trying to acquire lock is blocked

--- code/test/testmutex.c
+++ code/test/testmutex.c
@@ -10,6 +10,7 @@ int main(){
 	int lock = CreateLock("A lock", sizeof("A lock"));
 	int counter = CreateMV("Counter", sizeof("Counter"), 1, 0);
 	int i = 0;
+	int j = 0;
 	/*NPrint("about to acquire\n", sizeof("about to acquire\n"), 0, 0);*/
 	Acquire(lock);
 	/*NPrint("acquired, about to setMV\n", sizeof("acquired, about to setMV\n"), 0, 0);*/
@@ -17,6 +18,7 @@ int main(){
 	/*NPrint("setMV, about to loop\n", sizeof("setMV, about to loop\n"), 0, 0);*/
 	for(i = 0; i < 30; i++){
 		NPrint("counter value is %d\n", sizeof("counter value is %d\n"), GetMV(counter, 0), 0);
+		for(j = 0; j < 2000; j++){};
 	}
 	Release(lock);
 	/*NPrint("Released lock\n", sizeof("Released lock\n"));*/
