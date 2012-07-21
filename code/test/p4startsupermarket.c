79e27b326d3a778b0f173c04aec1f85a473279bc
laurencerwong@gmail.com <laurencerwong@gmail.com@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-21 23:19:28 +0000

    added salespeople for all departments.  was just for department 1 previously

--- code/test/p4startsupermarket.c
+++ code/test/p4startsupermarket.c
@@ -5,9 +5,15 @@ void main(){
   Exec("../test/manager", sizeof("../test/manager"), "manager thread", sizeof("manager thread"));
 
   /*Salesmen Next*/
-  Exec("../test/salesman", sizeof("../test/salesman"), "salesman0 thread", sizeof("salesman0 thread"));
-  Exec("../test/salesman", sizeof("../test/salesman"), "salesman1 thread", sizeof("salesman1 thread"));
-  Exec("../test/salesman", sizeof("../test/salesman"), "salesman2 thread", sizeof("salesman2 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman0-0 thread", sizeof("salesman0-0 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman0-1 thread", sizeof("salesman0-1 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman0-2 thread", sizeof("salesman0-2 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman1-0 thread", sizeof("salesman1-0 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman1-1 thread", sizeof("salesman1-1 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman1-2 thread", sizeof("salesman1-2 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman2-0 thread", sizeof("salesman2-0 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman2-1 thread", sizeof("salesman2-1 thread"));
+  Exec("../test/salesman", sizeof("../test/salesman"), "salesman2-2 thread", sizeof("salesman2-2 thread"));
 
 
   /*Cashiers Next*/
