3353fcc551d057183945f7eddd8898aedf37095c
laurencerwong@gmail.com <laurencerwong@gmail.com@0add23d7-9e33-bb4f-7084-8ec57f74e5f6>
2012-07-22 13:24:20 +0000

    part 1 works

--- trunk/code/network/post.cc
+++ trunk/code/network/post.cc
@@ -272,7 +272,6 @@ PostOffice::Send(PacketHeader pktHdr, MailHeader mailHdr, char* data)
 	printf("Post send: ");
 	PrintHeader(pktHdr, mailHdr);
     }
-    usleep(50);
     ASSERT(mailHdr.length <= MaxMailSize);
     ASSERT(0 <= mailHdr.to && mailHdr.to < numBoxes);
     
